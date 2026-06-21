027783d1dbc3bc613cd0c8f3e35c1b4372acfa3d
chris115713 <usher115713@gmail.com>
2026-06-21 13:12:20 +0800

    Update diy-part1.shCustomize feeds for N100 iStoreOS build

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -17,6 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+sed -i '1isrc-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '1isrc-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1isrc-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+
 # echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
